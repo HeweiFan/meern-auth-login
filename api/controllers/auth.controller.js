7d33c6cfb574920e36404975a2b7be84a866b725
“HeweiFan” <hw417090@dal.ca>
2023-09-17 15:43:49 -0300

    add signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-// import { errorHandler } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     //req is the data from client side
@@ -15,6 +16,26 @@ export const signup = async (req, res, next) => {
         // res.status(500).json({ message: error.message });
         next(error);
         // next(errorHandler(500, 'something went wrong'));
+    }
+
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(400, 'Wrong password'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        res
+          .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+          .status(200)
+          .json(rest);
+    } catch (error) {
+        next(error);
     }
 
 }

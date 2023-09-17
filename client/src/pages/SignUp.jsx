027423f08bba146aff2a75cea61a7e4f1bb5faee
“HeweiFan” <hw417090@dal.ca>
2023-09-17 15:01:42 -0300

    update signup component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,80 +1,81 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+// import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
-    // console.log(e.target.value);
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(e.target.value);
-  // }
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
       setError(false);
-      const res= await fetch("/api/auth/signup", {
-        method: "POST",
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       setLoading(false);
-      if (data.success === 'false') {
-        setError(false);
-        console.log(data);
+      if (data.success === false) {
+        setError(true);
+        return;
       }
+      navigate('/sign-in');
     } catch (error) {
-      setError(true);
       setLoading(false);
+      setError(true);
     }
-  }
-
+  };
   return (
-    <div className="mx-auto max-w-lg p-3">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="text"
-          id="username"
-          placeholder="Username"
-          className="bg-indigo-100 p-3 rounded-lg"
+          type='text'
+          placeholder='Username'
+          id='username'
+          className='bg-slate-100 p-3 rounded-lg'
           onChange={handleChange}
         />
         <input
-          type="text"
-          id="email"
-          placeholder="Email"
-          className="bg-indigo-100 p-3 rounded-lg"
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
           onChange={handleChange}
         />
         <input
-          type="text"
-          id="password"
-          placeholder="Password"
-          className="bg-indigo-100 p-3 rounded-lg"
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 p-3 rounded-lg uppercase hover:opacity-95 text-violet-50 text-center">
-          {loading ? "Loading..." : "Sign Up"}
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        {/* <OAuth /> */}
       </form>
-      <div className="flex py-3 gap-2">
-        <p>Have had a username?</p>
-        <Link to="/sign-in">
-          <span>Sign in</span>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
-      <p className="text-red-500">{error && "something went wrong"}</p>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
   );
 }
